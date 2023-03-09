9cc6cd94255575940ee551aab9f4cb160f50d03b
yousaf wazir <umbrellaman612@gmail.com>
2023-03-09 11:27:16 +0000

    added container

--- src/layouts/GlobalLayout.tsx
+++ src/layouts/GlobalLayout.tsx
@@ -4,10 +4,10 @@ import { ReactNode } from "react";
 
 export default function GlobalLayout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className="w-full h-full container mx-auto px-6 lg:px-0">
       <Navigation />
       <main className="w-full h-full">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
